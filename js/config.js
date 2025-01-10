a0b2d01be48047b3f048d46de56e2aab8a6925c1
tanueedee <146930070+tanueedee@users.noreply.github.com>
2025-01-10 20:19:39 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_fwGG0sPJ0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3digh475j6efoodqmcl57dmvr9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6sqa670dckr3t3e0eh338bp766', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-1  
     },
     api: {
